30b18ca618c2bfb94d3002e080de88089c1cccf5
zhanshenqian <1047315142@qq.com>
2017-03-22 14:39:06 +0800

    手机
    兼容手机rem布局

--- first/build/webpack.base.conf.js
+++ first/build/webpack.base.conf.js
@@ -96,5 +96,17 @@ module.exports = {
         browsers: ['last 2 versions']
       })
     ]
+      // require('autoprefixer')({
+      //   browsers: ["Android >= 4", "iOS >= 6"]  // 设置浏览器版本
+      // }),
+      // require('postcss-px2rem')({
+      //   baseDpr: 2,             // base device pixel ratio (default: 2)
+      //   threeVersion: false,    // whether to generate @1x, @2x and @3x version (default: false)
+      //   remVersion: true,       // whether to generate rem version (default: true)
+      //   remUnit: 75,            // rem unit value (default: 75)
+      //   remPrecision: 6,        // rem precision (default: 6)
+      //   forcePxComment: '!px',
+      //   keepComment: '!no'
+      // })]
   }
 }
